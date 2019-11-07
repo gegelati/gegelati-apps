e4d1d11db4857a5a96af037fa6b737800a5b4f1d
kdesnos <kdesnos@insa-rennes.fr>
2019-11-07 11:26:53 +0100

    (MNIST) MNIST reader code from wichtounet/mnist. (MIT License).

--- mnist/src/main.cpp
+++ mnist/src/main.cpp
@@ -2,7 +2,20 @@
 
 #include <gegelati.h>
 
+#include "mnist_reader/mnist_reader.hpp"
+
 int main() {
-	std::cout << "Hello TPG world" << std::endl;
+	// MNIST_DATA_LOCATION set by MNIST cmake config
+	std::cout << "MNIST data directory: " << MNIST_DATA_LOCATION << std::endl;
+
+	// Load MNIST data
+	mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset =
+		mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(MNIST_DATA_LOCATION);
+
+	std::cout << "Nbr of training images = " << dataset.training_images.size() << std::endl;
+	std::cout << "Nbr of training labels = " << dataset.training_labels.size() << std::endl;
+	std::cout << "Nbr of test images = " << dataset.test_images.size() << std::endl;
+	std::cout << "Nbr of test labels = " << dataset.test_labels.size() << std::endl;
+
 	return 0;
 }
