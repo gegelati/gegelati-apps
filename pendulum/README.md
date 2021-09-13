05fc9e2b978517e18a356eea056b9826006b5565
kdesnos <kdesnos@insa-rennes.fr>
2021-09-13 17:01:26 +0200

    (Pendulum) Add a new target running the pre-trained TPG.

--- pendulum/README.md
+++ pendulum/README.md
@@ -13,7 +13,8 @@ The build process of applications relies on [cmake](https://cmake.org) to config
 
 ## CodeGen example
 
-The folder src/CodeGen contains an example of use case for the code gen. There are 3 targets for this example, you can directly run the last one :
-- PendulumCodeGenCompile that import the TPG_graph.dot and launch the code gen to generate the sources files. If you want to run this target you need to set your working directory as the current build directory of your build system. You can use the following variable $CMakeCurrentBuildDir$.
-- PendulumCodeGenGenerate that is a custom command to execute the previous target 
-- PendulumCodeGenInference that use the generated file and link them with the learning environment of the directory src/Learn. 
+The folder src/CodeGen contains an example of use case for the code generation feature of GEGELATI. There are 4 targets for this example:
+- `pendulumCodeGenCompile`: Import the TPG_graph.dot and launch the code gen to generate the sources files. If you want to run this target you need to set your working directory as the current build directory of your build system. You can use the following variable $CMakeCurrentBuildDir$.
+- `pendulumCodeGenGenerate`: A custom command to execute the previous target (after it is compiled)
+- `pendulumCodeGenInference`: Uses the generated file and link them with the learning environment of the directory è `src/Learn`. This target depend on the previous, so building it will automatically trigger a build of the two previous.
+- `pendulumTPGInference`: Import the `TPG_graph.dot` and run it within the pendulum learning environment, in the exact same condition as within the `pendulumCodeGenGenerate` target. This target enables comparing the identical behavior of the generated code and the original TPG.
--- pendulum/src/CodeGen/mainCodeGenInference.cpp
+++ pendulum/src/CodeGen/mainCodeGenInference.cpp
@@ -17,6 +17,7 @@ extern "C" {
 int main() {
 	/// initialise AdversarialLearningEnvironment
 	auto le = Pendulum({ 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0 });
+	size_t seed = 2;
 
 	/// fetch data in the environment
 	auto dataSources = le.getDataSources();
@@ -24,27 +25,32 @@ int main() {
 	auto dataSharedPointer = st.getDataAt(typeid(double), 0).getSharedPointer<double>();
 	in1 = dataSharedPointer.get();
 
-	/// set the number of generation
-	uint64_t action;
-	in1[0] = M_PI;
+	/// Init the pendulum
+	le.reset(seed);
 	float angleDisplay = (float)(in1[0]);
 	float torqueDisplay = (float)(in1[1]);
 
 #ifndef NO_CONSOLE_CONTROL
 	Render::renderInit();
 	Render::renderEnv(&angleDisplay, &torqueDisplay, 0, 0);
+#else
+	auto colWidth(7);
+	std::cout << std::setprecision(2) << std::fixed << std::right;
+	std::cout << std::setw(colWidth) << "frame" << std::setw(colWidth) << "action";
+	std::cout << std::setw(colWidth) << "angle" << std::setw(colWidth) << "torque" << std::endl;
 #endif
 
 	// Do one inference
 	int nbActions = 0;
+	uint64_t actions[1000];
+	// measure time 
+	auto start = std::chrono::system_clock::now();
 	while (nbActions < 1000 && !le.isTerminal()) {
-		nbActions++;
 
 		/// inference with generated C files
-		action = inferenceTPG();
-
+		actions[nbActions] = inferenceTPG();
 		// Do the action 
-		le.doAction(action);
+		le.doAction(actions[nbActions]);
 
 		// Display the result
 		angleDisplay = (float)(in1[0]);
@@ -52,12 +58,39 @@ int main() {
 
 #ifndef NO_CONSOLE_CONTROL
 		Render::renderEnv(&angleDisplay, &torqueDisplay, nbActions, 0);
+#else
+		std::cout << std::setw(colWidth) << nbActions << std::setw(colWidth) << actions[nbActions];
+		std::cout << std::setw(colWidth) << in1[0] << std::setw(colWidth) << in1[1] << std::endl;
 #endif
-
-#ifdef NO_CONSOLE_CONTROL
-		std::cout << "TPG : " << action << " \t";
-		std::cout << "angle : " << in1[0] << " | float : " << angleDisplay << "\t";
-		std::cout << "torque : " << in1[1] << " | float : " << torqueDisplay << std::endl;
-#endif
+		nbActions++;
 	}
+	auto stop = std::chrono::system_clock::now();
+
+	// do a replay to subtract non-inference time
+	size_t iter = 0;
+	le.reset(seed);
+	// in1[0] = M_PI; // reset pendulum position
+	auto startReplay = std::chrono::system_clock::now();
+	while (iter < nbActions) {
+		// Do the action 
+		le.doAction(actions[iter]);
+
+		// Display the result
+		angleDisplay = (float)(in1[0]);
+		torqueDisplay = (float)(in1[1]);
+
+#ifndef NO_CONSOLE_CONTROL
+		Render::renderEnv(&angleDisplay, &torqueDisplay, nbActions, 0);
+#else
+		std::cout << std::setw(colWidth) << iter << std::setw(colWidth) << actions[iter];
+		std::cout << std::setw(colWidth) << in1[0] << std::setw(colWidth) << in1[1] << std::endl;
+#endif
+		iter++;
+	}
+	auto stopReplay = std::chrono::system_clock::now();
+	auto totalTime = ((std::chrono::duration<double>)(stop - start)).count();
+	auto replayTime = ((std::chrono::duration<double>)(stopReplay - startReplay)).count();
+	std::cout << std::setprecision(6) << " Total time: " << totalTime << std::endl;
+	std::cout << std::setprecision(6) << "  Env. time: " << replayTime << std::endl;
+	std::cout << std::setprecision(6) << "Infer. time: " << totalTime-replayTime << std::endl;
 }
