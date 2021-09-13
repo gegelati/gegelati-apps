326a7764026e33c7e4c12273770272eb4f634a5c
kdesnos <kdesnos@insa-rennes.fr>
2021-09-13 14:20:23 +0200

    (Pendulum) Mutualize instruction set between all pendulum projects.

--- pendulum/src/CodeGen/mainCodeGenCompile.cpp
+++ pendulum/src/CodeGen/mainCodeGenCompile.cpp
@@ -13,58 +13,35 @@
 
 #include <gegelati.h>
 
+#include "../Learn/instructions.h"
+
 
 int main() {
 
-    std::cout << "Generate C code from pre-trained dot file." << std::endl;
+	std::cout << "Generate C code from pre-trained dot file." << std::endl;
 
-    // Create the instruction set for programs
-    Instructions::Set set;
-    auto minus = [](double a, double b) -> double { return a - b; };
-    auto add = [](double a, double b) -> double { return a + b; };
-    auto mult = [](double a, double b) -> double { return a * b; };
-    auto div = [](double a, double b) -> double { return a / b; };
-    auto max = [](double a, double b) -> double { return std::max(a, b); };
-    auto ln = [](double a) -> double { return std::log(a); };
-    auto exp = [](double a) -> double { return std::exp(a); };
-    auto cos = [](double a) -> double { return std::cos(a); };
-    auto sin = [](double a) -> double { return std::sin(a); };
-    auto tan = [](double a) -> double { return std::tan(a); };
-    auto pi = [](double a) -> double { return M_PI; };
-    auto multByConst = [](double a, Data::Constant c) -> double { return a * (double) c / 10.0; };
+	// Create the instruction set for programs
+	Instructions::Set set;
+	fillInstructionSet(set);
 
-    set.add(*(new Instructions::LambdaInstruction<double, double>(minus, "$0 = $1 - $2;")));
-    set.add(*(new Instructions::LambdaInstruction<double, double>(add, "$0 = $1 + $2;")));
-    set.add(*(new Instructions::LambdaInstruction<double, double>(mult, "$0 = $1 * $2;")));
-    set.add(*(new Instructions::LambdaInstruction<double, double>(div, "$0 = $1 / $2;")));
-    set.add(*(new Instructions::LambdaInstruction<double, double>(max, "$0 = (($1) < ($2)) ? ($2) : ($1);")));
-    set.add(*(new Instructions::LambdaInstruction<double>(exp, "$0 = exp($1);")));
-    set.add(*(new Instructions::LambdaInstruction<double>(ln, "$0 = log($1);")));
-    set.add(*(new Instructions::LambdaInstruction<double>(cos, "$0 = cos($1);")));
-    set.add(*(new Instructions::LambdaInstruction<double>(sin, "$0 = sin($1);")));
-    set.add(*(new Instructions::LambdaInstruction<double>(tan, "$0 = tan($1);")));
-    set.add(*(new Instructions::LambdaInstruction<double, Data::Constant>(multByConst,
-                                                                          "$0 = $1 * ((double)($2) / 10.0);")));
-    set.add(*(new Instructions::LambdaInstruction<double>(pi, "$0 = M_PI;")));
+	std::string path(ROOT_DIR "/src/CodeGen/");
 
-    std::string path(ROOT_DIR "/src/CodeGen/");
+	Data::PrimitiveTypeArray<double> currentState{ 2 };
+	std::vector<std::reference_wrapper<const Data::DataHandler>> data = { currentState };
 
-    Data::PrimitiveTypeArray<double> currentState{2};
-    std::vector<std::reference_wrapper<const Data::DataHandler>> data = {currentState};
+	Learn::LearningParameters params;
+	File::ParametersParser::loadParametersFromJson(
+		ROOT_DIR"/params.json", params);
+	Environment dotEnv(set, data, params.nbRegisters, params.nbProgramConstant);
+	TPG::TPGGraph dotGraph(dotEnv);
 
-    Learn::LearningParameters params;
-    File::ParametersParser::loadParametersFromJson(
-            ROOT_DIR"/params.json", params);
-    Environment dotEnv(set, data, params.nbRegisters, params.nbProgramConstant);
-    TPG::TPGGraph dotGraph(dotEnv);
+	std::string filename(path + "Pendulum_out_best.dot");
+	File::TPGGraphDotImporter dot(filename.c_str(), dotEnv, dotGraph);
+	dot.importGraph();
 
-    std::string filename(path + "Pendulum_out_best.dot");
-    File::TPGGraphDotImporter dot(filename.c_str(), dotEnv, dotGraph);
-    dot.importGraph();
-
-    CodeGen::TPGGenerationEngine tpggen("pendulum", dotGraph, "src/");
-    tpggen.generateTPGGraph();
+	CodeGen::TPGGenerationEngine tpggen("pendulum", dotGraph, "src/");
+	tpggen.generateTPGGraph();
 
 
-    return 0;
+	return 0;
 }
--- pendulum/src/CodeGen/mainCodeGenInference.cpp
+++ pendulum/src/CodeGen/mainCodeGenInference.cpp
@@ -4,6 +4,7 @@
 
 #include "../Learn/pendulum.h"
 #include "../Learn/render.h"
+#include "../Learn/instructions.h"
 
 extern "C" {
 #include "pendulum.h"
@@ -15,31 +16,7 @@ extern "C" {
 int main() {
 	/// Import instruction set used during training(required only for gegelati Inference)
 	Instructions::Set set;
-	auto minus = [](double a, double b) -> double { return a - b; };
-	auto add = [](double a, double b) -> double { return a + b; };
-	auto mult = [](double a, double b) -> double { return a * b; };
-	auto div = [](double a, double b) -> double { return a / b; };
-	auto max = [](double a, double b) -> double { return std::max(a, b); };
-	auto ln = [](double a) -> double { return std::log(a); };
-	auto exp = [](double a) -> double { return std::exp(a); };
-	auto cos = [](double a) -> double { return std::cos(a); };
-	auto sin = [](double a) -> double { return std::sin(a); };
-	auto tan = [](double a) -> double { return std::tan(a); };
-	auto pi = [](double a) -> double { return M_PI; };
-	auto multByConst = [](double a, Data::Constant c) -> double { return a * (double)c / 10.0; };
-
-	set.add(*(new Instructions::LambdaInstruction<double, double>(minus)));
-	set.add(*(new Instructions::LambdaInstruction<double, double>(add)));
-	set.add(*(new Instructions::LambdaInstruction<double, double>(mult)));
-	set.add(*(new Instructions::LambdaInstruction<double, double>(div)));
-	set.add(*(new Instructions::LambdaInstruction<double, double>(max)));
-	set.add(*(new Instructions::LambdaInstruction<double>(exp)));
-	set.add(*(new Instructions::LambdaInstruction<double>(ln)));
-	set.add(*(new Instructions::LambdaInstruction<double>(cos)));
-	set.add(*(new Instructions::LambdaInstruction<double>(sin)));
-	set.add(*(new Instructions::LambdaInstruction<double>(tan)));
-	set.add(*(new Instructions::LambdaInstruction<double, Data::Constant>(multByConst)));
-	set.add(*(new Instructions::LambdaInstruction<double>(pi)));
+	fillInstructionSet(set);
 
 	/// initialise AdversarialLearningEnvironment
 	auto le = Pendulum({ 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0 });
