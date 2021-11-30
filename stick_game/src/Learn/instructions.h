5fc39d69f2894ff4409190c91c3a714778c17786
kdesnos <kdesnos@insa-rennes.fr>
2021-11-30 15:09:01 +0100

    (RelEng) Clean comments.

--- stick_game/src/Learn/instructions.h
+++ stick_game/src/Learn/instructions.h
@@ -2,7 +2,7 @@
 * The purpose of this file is to provide a utility function
 * filling a given Instructions::Set with instruction.
 * The objective of this file is to specify the instructions
-* used in all pendulum related project in a single place.
+* used in all stick_game related project in a single place.
 */
 
 #ifndef INSTRUCTIONS_H
