7860b2dc80705f62692903dbdc010d2ad2b0de1d
Quentin Vacher <quentinvcher@gmail.com>
2025-07-13 03:23:14 +0200

    Less files

--- mujoco/src/mainRender.cpp
+++ mujoco/src/mainRender.cpp
@@ -295,7 +295,7 @@ int main(int argc, char ** argv) {
     strcat(clearDot, ".clear.dot");
     File::TPGGraphDotExporter dotExporter(clearDot, *la.getTPGGraph());
     dotExporter.print();
-    std::cout<<"Save cleared root in "<<clearDot<<" --- ";
+    std::cout<<"Save cleared root in "<<dotPath<<" --- ";
 
     // Print graph
     if(saveCodeGen){
